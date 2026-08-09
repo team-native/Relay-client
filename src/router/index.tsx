255ed5f29527f05723191c069da30dc38c0ec3f0
(i1anzu) <(s26040@gsm.hs.kr)>
2026-08-10 01:47:04 +0900

    fix: 새 게시물 api 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,6 +7,8 @@ import NoticeDetailPage from '../pages/notice/NoticeDetailPage';
 import MyPage from '../pages/mypage/MyPage';
 import ProfileEditPage from '../pages/mypage/ProfileEditPage';
 import PasswordChangePage from '../pages/mypage/PasswordChangePage';
+import StudyCreatePage from '../pages/study/StudyCreatePage';
+import StudyDetailPage from '../pages/study/StudyDetailPage';
 
 import Login from '../pages/Login/Login';
 import Signup from '../pages/Signup/Signup';
@@ -23,8 +25,11 @@ export const router = createBrowserRouter([
     errorElement: <RouteErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'notices', element: <NoticeListPage /> },
-      { path: 'notices/:noticeId', element: <NoticeDetailPage /> },
+      { path: 'home', element: <HomePage /> },
+      { path: 'new', element: <StudyCreatePage /> },
+      { path: 'lecture/:studyId', element: <StudyDetailPage /> },
+      { path: 'notice', element: <NoticeListPage /> },
+      { path: 'notice/:noticeId', element: <NoticeDetailPage /> },
       { path: 'mypage', element: <MyPage /> },
       { path: 'mypage/profile', element: <ProfileEditPage /> },
       { path: 'mypage/password', element: <PasswordChangePage /> },
