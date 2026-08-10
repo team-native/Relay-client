c4e3717ce451ec54ca6796161c8cbe479efef536
(i1anzu) <(s26040@gsm.hs.kr)>
2026-08-10 22:41:53 +0900

    fix: 공지사항 관련 라우트 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import RouteErrorPage from '../components/layout/RouteErrorPage';
 import HomePage from '../pages/home/HomePage';
 import NoticeListPage from '../pages/notice/NoticeListPage';
 import NoticeDetailPage from '../pages/notice/NoticeDetailPage';
+import NoticeFormPage from '../pages/notice/NoticeFormPage'; // 👈 1. Import 추가!
 import MyPage from '../pages/mypage/MyPage';
 import ProfileEditPage from '../pages/mypage/ProfileEditPage';
 import PasswordChangePage from '../pages/mypage/PasswordChangePage';
@@ -28,8 +29,13 @@ export const router = createBrowserRouter([
       { path: 'home', element: <HomePage /> },
       { path: 'new', element: <StudyCreatePage /> },
       { path: 'lecture/:studyId', element: <StudyDetailPage /> },
+      
+      // 📌 공지사항 관련 라우트 수정
       { path: 'notice', element: <NoticeListPage /> },
+      { path: 'notice/new', element: <NoticeFormPage /> }, // 👈 2. 공지 작성 (상세페이지보다 위에!)
       { path: 'notice/:noticeId', element: <NoticeDetailPage /> },
+      { path: 'notice/:noticeId/edit', element: <NoticeFormPage /> }, // 👈 3. 공지 수정
+      
       { path: 'mypage', element: <MyPage /> },
       { path: 'mypage/profile', element: <ProfileEditPage /> },
       { path: 'mypage/password', element: <PasswordChangePage /> },
